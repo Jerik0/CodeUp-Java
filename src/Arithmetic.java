4853081ca14aa1263f174a611f93bf29d4e776f1
jerik0 <jcarrico1985@live.com>
2017-05-08 14:56:15 -0500

    initialized hello-world project

---
+++ src/Arithmetic.java
@@ -0,0 +1,19 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        double number1 = 12;
+        double number2 = 6.5;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number1 / number2);
+
+        int number3 = 10;
+        int number4 = 4;
+
+        double number3Larger = number3;
+        System.out.println(number3Larger);
+
+        System.out.println(number3 % number4);
+        System.out.println(number3 / number4); // The division will be an integer only.
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,56 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Zelda Breath of The Wild is Amazing!!";
+        System.out.println(myString);
+
+//        char myString = 'c';
+//        System.out.println(myString);
+//
+//        double myString = 3.14159;
+//        System.out.println(myString);
+
+//        long myNumber;
+
+//        myNumber = 3.14; //possible lossy conversion from double to long
+//        System.out.println(myNumber);
+
+
+
+//        myNumber = 123L; //no errors like this.
+//        System.out.println(myNumber);
+//
+//        myNumber = 123; //no errors like this.
+//        System.out.println(myNumber);
+
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        int x = 4;
+        x += 5;
+
+        int x = 3;
+        int y = 4;
+        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+
+
+    }
+}
