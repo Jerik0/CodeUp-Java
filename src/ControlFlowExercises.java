94dd76bbe49676ff002ec399763e086ffc70967a
jerik0 <jcarrico1985@live.com>
2017-05-10 11:17:22 -0500

    Finished Letter Grade Exercise.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,21 +5,31 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
 
         //Loop Basics
-        //Display Squared and Cubed Table
+        //Display Letter Grade based on numerical input
 
     Scanner scan = new Scanner(System.in);
 
-    System.out.println("Choose a number, 127 or lower, to be squared and cubed.");
+
+    String cont;
+        do {
+    System.out.println("Input a numerical grade, from 0-100, to see the Letter Grade associated with it.");
     int input = scan.nextInt();
-    int i;
+        if (input <= 59) {
+            System.out.println("The corresponding Letter Grade is: F");
+        } else if (input <= 66) {
+            System.out.println("The corresponding Letter Grade is: D");
+        } else if (input <= 79) {
+            System.out.println("The corresponding Letter Grade is: C");
+        } else if (input <= 87) {
+            System.out.println("The corresponding Letter Grade is: B");
+        } else {
+            System.out.println("The corresponding Letter Grade is: A");
+        }
+        scan.nextLine();
+    System.out.println("Continue? Y/N");
+        cont = scan.nextLine();
+    } while (cont.equals("y"));
 
-    System.out.println("Here is your table for number " + input + "!");
-    System.out.println("Number | Squared | Cubed");
-    System.out.println("------ | ------- | -----");
-    for(i = 1; i<=input; i++) {
-
-        System.out.println(i + "      | " + (i*i) + "       | " + (i*i*i) + "     ");
-    }
 
     }
 
