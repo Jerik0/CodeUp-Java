50cbd3e628369a789a73d32c78f3ced79a30a17a
jerik0 <jcarrico1985@live.com>
2017-05-11 16:16:06 -0500

    Added modulus function and implemented to user output.

--- src/Methods/Arithmetic.java
+++ src/Methods/Arithmetic.java
@@ -4,25 +4,30 @@ import java.util.Scanner;
 
 public class Arithmetic {
 
-    public static int add(int first, int second) {
+    private static int add(int first, int second) {
         int result = (first + second);
         return result;
     }
 
-    public static int subtract(int first, int second) {
+    private static int subtract(int first, int second) {
         int result = (first - second);
         return result;
     }
 
-    public static int multiply(int first, int second) {
+    private static int multiply(int first, int second) {
         int result = (first * second);
         return result;
     }
 
-    public static double divide(double first, double second) {
+    private static double divide(double first, double second) {
         double result = (first / second);
-            return result;
+        return result;
 
+    }
+
+    private static int modulus(int first, int second) {
+        int result = (first % second);
+        return result;
     }
 
     public static void main(String[] args) {
@@ -30,6 +35,7 @@ public class Arithmetic {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Type the two numbers (without commas) to perform mathematical functions on: ");
+        System.out.println("If you put the second number as zero, some operations won't work!");
         int Number1 = scan.nextInt();
         int Number2 = scan.nextInt();
 
@@ -38,6 +44,7 @@ public class Arithmetic {
         System.out.println("Multiplied: (" + Number1 + " x " + Number2 + ") = " + multiply(Number1, Number2));
         if (Number2 != 0) {
         System.out.println("Divided: " + Number1 + "/" + Number2 + " = " + divide(Number1, Number2));
+        System.out.println("Division Remainder (Modulus): " + Number1 + " % " + Number2 + " = " + modulus(Number1, Number2));
         } else {
             System.out.println("We can't divide by zero, silly!");
         }
