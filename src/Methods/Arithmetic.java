aa42be3a2f1871ca592dec337d202625e7f71e2f
jerik0 <jcarrico1985@live.com>
2017-05-11 15:44:53 -0500

    Edited divide function to give a double result instead of an int. also added the actual output to the user.

--- src/Methods/Arithmetic.java
+++ src/Methods/Arithmetic.java
@@ -19,14 +19,29 @@ public class Arithmetic {
         return result;
     }
 
-    public static int divide(int first, int second) {
-        int result = (first / second);
-        return result;
+    public static double divide(double first, double second) {
+        double result = (first / second);
+            return result;
+
     }
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Type the two numbers (without commas) to perform mathematical functions on: ");
+        int Number1 = scan.nextInt();
+        int Number2 = scan.nextInt();
+
+        System.out.println("Added together: " + Number1 + " + " + Number2 + " = " + add(Number1, Number2));
+        System.out.println("Subtracted: " + Number1 + " - " + Number2 + " = " + subtract(Number1, Number2));
+        System.out.println("Multiplied: (" + Number1 + " x " + Number2 + ") = " + multiply(Number1, Number2));
+        if (Number2 != 0) {
+        System.out.println("Divided: " + Number1 + "/" + Number2 + " = " + divide(Number1, Number2));
+        } else {
+            System.out.println("We can't divide by zero, silly!");
+        }
+
 
     }
 }
