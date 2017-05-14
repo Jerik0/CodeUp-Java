679f66eb234e3a4caf1577c6e77143ffa3452ae6
jerik0 <jcarrico1985@live.com>
2017-05-14 00:24:41 -0500

    Factor now works properly.

--- src/Methods/Arithmetic.java
+++ src/Methods/Arithmetic.java
@@ -4,50 +4,88 @@ import java.util.Scanner;
 
 public class Arithmetic {
 
+    private static Scanner scan = new Scanner(System.in);
+
+    //Create four separate methods. Each will perform an arithmetic operation:
     private static int add(int first, int second) {
         int result = (first + second);
+        System.out.println("Added together: " + first + " + " + second + " = " + result);
         return result;
     }
 
     private static int subtract(int first, int second) {
         int result = (first - second);
+        System.out.println("Subtracted: " + first + " - " + second + " = " + result);
         return result;
     }
 
     private static int multiply(int first, int second) {
         int result = (first * second);
+        System.out.println("Multiplied: (" + first + " x " + second + ") = " + result);
         return result;
     }
 
     private static double divide(double first, double second) {
         double result = (first / second);
-        return result;
-
-    }
-
-    private static int modulus(int first, int second) {
-        int result = (first % second);
-        return result;
-    }
-
-    public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Type the two numbers (without commas) to perform mathematical functions on: ");
-        System.out.println("If you put the second number as zero, some operations won't work!");
-        int Number1 = scan.nextInt();
-        int Number2 = scan.nextInt();
-
-        System.out.println("Added together: " + Number1 + " + " + Number2 + " = " + add(Number1, Number2));
-        System.out.println("Subtracted: " + Number1 + " - " + Number2 + " = " + subtract(Number1, Number2));
-        System.out.println("Multiplied: (" + Number1 + " x " + Number2 + ") = " + multiply(Number1, Number2));
-        if (Number2 != 0) {
-        System.out.println("Divided: " + Number1 + "/" + Number2 + " = " + divide(Number1, Number2));
-        System.out.println("Division Remainder (Modulus): " + Number1 + " % " + Number2 + " = " + modulus(Number1, Number2));
+        if (second != 0) {
+            System.out.println("Divided: " + first + "/" + second + " = " + result);
         } else {
             System.out.println("We can't divide by zero, silly!");
         }
+        return result;
+    }
+
+    //Add a modulus function that finds the modulus of two numbers.
+    private static int modulus(int first, int second) {
+        int result = (first % second);
+        if (second != 0) {
+        System.out.println("Division Remainder (Modulus): " + first + " % " + second + " = " + result);
+        } else {
+            System.out.println("We can't divide by zero, silly!");
+        }
+        return result;
+    }
+
+    //Create a method that validates that user input is in a certain range
+    private static int getIntegers(int min, int max) {
+
+        if (min < 1 && max > 10) {
+            System.out.println("Invalid number, try again!");
+            return getIntegers(min, max);
+        } else {
+            System.out.println("Thanks!");
+            return 100;
+        }
+    }
+
+    private static int factor(int n) {
+        int result;
+            if (n < 1 || n > 10) {
+                System.out.println("Whoops that wasn't in the range!");
+                return 1;
+            } else {
+                result = factor(n-1) * n;
+                System.out.println(n + "!" + " = " + result);
+                return result;
+            }
+        }
+
+    public static void main(String[] args) {
+
+        System.out.println("Type two numbers (between 1 and 10) to perform mathematical functions on.");
+        int input1 = scan.nextInt();
+        int input2 = scan.nextInt();
+        getIntegers(input1, input2);
+        add(input1, input2);
+        subtract(input1, input2);
+        multiply(input1, input2);
+        divide(input1, input2);
+        modulus(input1, input2);
+
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Type a number (between 1 and 10) to find the factorial for.");
+        int a = scan.nextInt();
+        factor(a);
 
 
     }
