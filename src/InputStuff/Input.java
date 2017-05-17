226a8bcdefeef2660cacfee7895f7f21a1cc0686
jerik0 <jcarrico1985@live.com>
2017-05-17 14:16:33 -0500

    Completed Input.java

--- src/InputStuff/Input.java
+++ src/InputStuff/Input.java
@@ -7,42 +7,54 @@ public class Input {
     private Scanner scanner;
 
     public Input() {
-
         this.scanner = new Scanner(System.in);
-
     }
 
     public String getString() {
-
-        return "this is a string yay";
+        String input;
+        System.out.println("Gimme a sentence or a word!");
+        input = scanner.nextLine();
+        return input;
     }
 
-    public boolean yesNo(String input) {
-
+    public boolean yesNo() {
+        System.out.println("Please enter yes or no");
+        String input = scanner.nextLine();
         if(input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
             return true;
         }
-
         return false;
     }
 
-    public int getInt(int min, int max) {
-
-        return 0;
-    }
-
     public int getInt() {
+        int min = 0;
+        int max = 10;
 
-        return 0;
-    }
+        int input;
+        System.out.println("Please enter a whole number!");
+        input = scanner.nextInt();
 
-    public double getDouble(double min, double max) {
-
+        if (input > min && input <= max) {
+            return (input);
+        } else {
+            getInt();
+        }
         return 0;
     }
 
     public double getDouble() {
+        int min = 0;
+        int max = 10;
 
+        double input;
+        System.out.println("Please enter a decimal number!");
+        input = scanner.nextDouble();
+
+        if (input > min && input <= max) {
+            return (input);
+        } else {
+            getInt();
+        }
         return 0;
     }
 
--- src/InputStuff/InputTest.java
+++ src/InputStuff/InputTest.java
@@ -1,20 +1,26 @@
 package InputStuff;
 
-import java.util.Scanner;
+import java.io.PrintStream;
 
 public class InputTest {
 
 
     public static void main(String[] args) {
 
+        String separator = "================================";
         Input testie = new Input();
+        PrintStream print = System.out;
 
-        testie.yesNo(String input);
-        testie.getString();
-        testie.getInt();
-        testie.getInt();
-        testie.getDouble();
-        testie.getDouble();
+        print.println(testie.yesNo());
+        print.println(separator);
+        print.println(testie.getString());
+        print.println(separator);
+        print.println("Thanks! I got " + testie.getInt());
+        print.println(separator);
+        print.println("Thanks! You entered: " + testie.getInt());
+        print.println(separator);
+        print.println("Thanks! You entered: " + testie.getDouble());
+        print.println("*/*/*/*/*/Thanks for playing!*/*/*/*/*/");
 
     }
 }
